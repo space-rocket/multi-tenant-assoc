232b540946677a74351618acd79d54c729e36b8f
admin <bigbassroller@gmail.com>
2020-05-29 05:52:41 -0700

    append to last commit

--- lib/my_app_web/controllers/subdomain/user_controller.ex
+++ lib/my_app_web/controllers/subdomain/user_controller.ex
@@ -5,7 +5,8 @@ defmodule MyAppWeb.Subdomain.UserController do
   alias MyApp.Accounts.User
 
   def index(conn, _params) do
-    users = Accounts.list_users()
+    tenant = conn.private[:subdomain]
+    users = Accounts.list_users(tenant)
     render(conn, "index.html", users: users)
   end
 
@@ -15,7 +16,8 @@ defmodule MyAppWeb.Subdomain.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    tenant = conn.private[:subdomain]
+    case Accounts.create_user(user_params, tenant) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "User created successfully.")
@@ -27,20 +29,23 @@ defmodule MyAppWeb.Subdomain.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
+    tenant = conn.private[:subdomain]
+    user = Accounts.get_user!(id, tenant)
     render(conn, "show.html", user: user)
   end
 
   def edit(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
+    tenant = conn.private[:subdomain]
+    user = Accounts.get_user!(id, tenant)
     changeset = Accounts.change_user(user)
     render(conn, "edit.html", user: user, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
+    tenant = conn.private[:subdomain]
+    user = Accounts.get_user!(id, tenant)
 
-    case Accounts.update_user(user, user_params) do
+    case Accounts.update_user(user, user_params, tenant) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "User updated successfully.")
@@ -52,8 +57,9 @@ defmodule MyAppWeb.Subdomain.UserController do
   end
 
   def delete(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    {:ok, _user} = Accounts.delete_user(user)
+    tenant = conn.private[:subdomain]
+    user = Accounts.get_user!(id, tenant)
+    {:ok, _user} = Accounts.delete_user(user, tenant)
 
     conn
     |> put_flash(:info, "User deleted successfully.")
